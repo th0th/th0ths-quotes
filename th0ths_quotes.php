fa954866d018d9dfcd79a62c165830a10d63bea3
H.Gökhan Sarı <th0th@returnfalse.net>
2011-05-24 01:54:48 +0300

    Import from github.

---
+++ th0ths_quotes.php
@@ -0,0 +1,172 @@
+<?php
+
+/*
+Plugin Name: th0th's quotes
+Plugin URI: http://URI_Of_Page_Describing_Plugin_and_Updates
+Description: Widget for displaying random quotes from your collection.
+Version: 0.1
+Author: H.Gökhan Sarı
+Author URI: http://returnfalse.net
+License: GPL3
+*/
+
+/*  Copyright 2011 Hüseyin Gökhan Sarı  (email : th0th -at- returnfalse.net)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as 
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+global $wpdb;
+global $th0ths_quotes_plugin_table;
+
+$th0ths_quotes_plugin_table = $wpdb->prefix . "th0ths_quotes";
+
+/* Plugin activation function */
+function th0ths_quotes_activate()
+{
+    global $wpdb, $th0ths_quotes_plugin_table;
+    
+    if($wpdb->get_var("SHOW TABLES LIKE '$th0ths_quotes_plugin_table'") != $th0ths_quotes_plugin_table)
+    {
+        $sql = "CREATE TABLE " . $th0ths_quotes_plugin_table . " (
+          id INT(12) NOT NULL AUTO_INCREMENT,
+          quote VARCHAR(255) NOT NULL,
+          owner VARCHAR(100) NOT NULL,
+          UNIQUE KEY id (id)
+        );";
+        
+        require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
+        dbDelta($sql);
+        
+        // Let's add some inital quotes.
+        $inital_quotes = array();
+        
+        $initial_quotes[] = array(
+            "quote" => "Happiness is a shadow of harmony; it follows harmony. There is no other way to be happy.",
+            "owner" => "Osho"
+        );
+        
+        $initial_quotes[] = array(
+            "quote" => "It is better to travel well than to arrive.",
+            "owner" => "Buddha"
+        );
+        
+        foreach ($initial_quotes as $quote)
+        {
+            $row = $wpdb->insert($th0ths_quotes_plugin_table, $quote);
+        }
+        
+        add_option("th0ths_quotes_version", "0.1");
+    }
+}
+
+/* Administration menus */
+function th0ths_quotes_add_administration_menus()
+{
+    /* add menu item */
+    add_menu_page("th0th's Quotes", "th0th's Quotes", "manage_options", "th0ths_quotes", "th0ths_quotes_manage_quotes");
+    
+    /* add submenu item */
+    add_submenu_page("th0ths_quotes", "th0th's Quotes", "th0th's Quotes", "manage_options", "th0ths_quotes", "th0ths_quotes_manage_quotes");
+}
+
+/* Adding CSS */
+function th0ths_quotes_include_css()
+{
+    ?>
+    <link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo('wpurl'); ?>/wp-content/plugins/th0ths_quotes/th0ths_quotes.css" />
+    <?php
+}
+
+/* Quote management page */
+function th0ths_quotes_manage_quotes()
+{
+	global $wpdb, $th0ths_quotes_plugin_table;
+	
+	if (!empty($_POST))
+	{
+		$new_quote = array(
+			'quote' => $_POST['quote'],
+			'owner' => $_POST['owner']
+		);
+		
+		$wpdb->insert($th0ths_quotes_plugin_table, $new_quote);
+	}
+	elseif (isset($_GET['action']))
+	{
+		if ($_GET['action'] == 'delete') {
+			$id = $_GET['id'];
+			
+			$wpdb->query("DELETE FROM $th0ths_quotes_plugin_table WHERE id = '$id'");
+		}
+	}
+		
+	
+	$quotes = $wpdb->get_results('SELECT * FROM ' . $th0ths_quotes_plugin_table, 'ARRAY_A');
+	
+    echo "<h2>" . __("th0th's Quotes Management") . "</h2>";
+    ?>
+    
+    <div class="wrap">
+		<table id="th0ths_quotes_qlist" class="widefat">
+			<thead>
+				<tr>
+					<th class="deleteLink">Delete</th>
+					<th>Quote</th>
+					<th>Owner</th>
+				</tr>
+				<?php foreach ($quotes as $quote) { ?>
+				<tr>
+					<td class="deleteLink"><a href="<?php echo add_query_arg(array('action' => 'delete', 'id' => $quote['id'])); ?>">[x]</a></td>
+					<td class="quote"><?php echo $quote['quote']; ?></td>
+					<td class="owner"><?php echo $quote['owner']; ?></td>
+				</tr>
+				<?php } ?>
+			</thead>
+		</table>
+		<form method="post">
+			<h3 class="hndle">Add new quote</h3>
+			<div id="th0ths_quotes_new_quote" class="postbox">
+				<span>Quote</span>
+				<div class="th0ths_quotes_cleanser"></div>
+				
+				<textarea name="quote"></textarea>
+				<div class="th0ths_quotes_cleanser"></div>
+				
+				<span>Owner</span>
+				<div class="th0ths_quotes_cleanser"></div>
+				
+				<input type="text" name="owner" />
+				<div class="th0ths_quotes_cleanser"></div>
+				
+				<input class="button" type="submit" value="Add" />
+			</div>
+		</form>
+    </div>
+    
+    <?php
+}
+
+
+/* registering functions */
+register_activation_hook(__FILE__, 'th0ths_quotes_activate');
+
+/* registering administration options */
+add_action('admin_menu', 'th0ths_quotes_add_administration_menus');
+
+/* add plugin stylesheet to wp_head */
+add_action('admin_head', 'th0ths_quotes_include_css');
+
+include "th0ths_quotes_widget.php";
+
+?>
---
+++ th0ths_quotes_widget.php
@@ -0,0 +1,61 @@
+<?php
+
+/*  Copyright 2011 Hüseyin Gökhan Sarı  (email : th0th -at- returnfalse.net)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 3, as 
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+class th0ths_Quotes_Widget extends WP_Widget {
+    function th0ths_quotes_widget() {
+        parent::WP_Widget(false, $name = 'Quotes');    
+    }
+
+    function widget($args, $instance) {
+		global $wpdb, $th0ths_quotes_plugin_table;
+        extract( $args );
+        $title = apply_filters('widget_title', $instance['title']);
+        $quotes = $wpdb->get_results('SELECT * FROM ' . $th0ths_quotes_plugin_table, 'ARRAY_A');
+        $quote = $quotes[array_rand($quotes)];
+        ?>
+              <?php echo $before_widget; ?>
+                  <?php if ( $title ) {
+                            echo $before_title . $title . $after_title; }
+                        else {
+                                echo $before_title . "Quotes" . $after_title; } ?>
+                  <?php echo $quote['quote']; ?> <i>-<?php echo $quote['owner']; ?></i>
+              <?php echo $after_widget; ?>
+        <?php
+    }
+
+    function update($new_instance, $old_instance) {
+    $instance = $old_instance;
+    $instance['title'] = strip_tags($new_instance['title']);
+        return $instance;
+    }
+
+    function form($instance) {
+        $title = esc_attr($instance['title']);
+        ?>
+         <p>
+          <label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Title:'); ?></label> 
+          <input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo $title; ?>" />
+        </p>
+        <?php 
+    }
+
+}
+
+add_action('widgets_init', create_function('', 'return register_widget("th0ths_Quotes_Widget");'));
+
+?>
