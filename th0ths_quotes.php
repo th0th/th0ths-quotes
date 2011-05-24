edfb8b17cf867a238cce0e11019e30e98b97a6a4
H.Gökhan Sarı <th0th@returnfalse.net>
2011-05-24 21:43:46 +0300

    New version 0.2

--- th0ths_quotes.php
+++ th0ths_quotes.php
@@ -4,7 +4,7 @@
 Plugin Name: th0th's quotes
 Plugin URI: http://URI_Of_Page_Describing_Plugin_and_Updates
 Description: Widget for displaying random quotes from your collection.
-Version: 0.1
+Version: 0.2
 Author: H.Gökhan Sarı
 Author URI: http://returnfalse.net
 License: GPL3
@@ -70,6 +70,13 @@ function th0ths_quotes_activate()
     }
 }
 
+/* JS */
+function th0ths_quotes_admin_head_js()
+{
+	?>
+<script type="text/javascript" src="<?php echo WP_PLUGIN_URL; ?>/th0ths_quotes/admin_head.js"></script>
+<?php }
+
 /* Administration menus */
 function th0ths_quotes_add_administration_menus()
 {
@@ -84,7 +91,7 @@ function th0ths_quotes_add_administration_menus()
 function th0ths_quotes_include_css()
 {
     ?>
-    <link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo('wpurl'); ?>/wp-content/plugins/th0ths_quotes/th0ths_quotes.css" />
+    <link rel="stylesheet" type="text/css" media="all" href="<?php echo WP_PLUGIN_URL; ?>/th0ths_quotes/th0ths_quotes.css" />
     <?php
 }
 
@@ -93,24 +100,32 @@ function th0ths_quotes_manage_quotes()
 {
 	global $wpdb, $th0ths_quotes_plugin_table;
 	
-	if (!empty($_POST))
+	if (!empty($_POST)) /* Action can be both add or delete */
 	{
-		$new_quote = array(
-			'quote' => $_POST['quote'],
-			'owner' => $_POST['owner']
-		);
-		
-		$wpdb->insert($th0ths_quotes_plugin_table, $new_quote);
-	}
-	elseif (isset($_GET['action']))
-	{
-		if ($_GET['action'] == 'delete') {
-			$id = $_GET['id'];
+		if ($_POST['action'] == 'addQuote')
+		{
+			$new_quote = array (
+				'quote' => $_POST['quote'],
+				'owner' => $_POST['owner']
+			);
 			
-			$wpdb->query("DELETE FROM $th0ths_quotes_plugin_table WHERE id = '$id'");
+			if (!(empty($new_quote['quote']) || empty($new_quote['owner'])))
+			{
+				$wpdb->insert($th0ths_quotes_plugin_table, $new_quote);
+			}
+			else
+			{
+				echo "error";
+			}
+		}
+		elseif ($_POST['action'] == 'delQuote')
+		{
+			foreach ($_POST['delQuoteID'] as $id)
+			{
+				$wpdb->query("DELETE FROM $th0ths_quotes_plugin_table WHERE id = '$id'");
+			}
 		}
 	}
-		
 	
 	$quotes = $wpdb->get_results('SELECT * FROM ' . $th0ths_quotes_plugin_table, 'ARRAY_A');
 	
@@ -118,22 +133,28 @@ function th0ths_quotes_manage_quotes()
     ?>
     
     <div class="wrap">
-		<table id="th0ths_quotes_qlist" class="widefat">
-			<thead>
-				<tr>
-					<th class="deleteLink">Delete</th>
-					<th>Quote</th>
-					<th>Owner</th>
-				</tr>
-				<?php foreach ($quotes as $quote) { ?>
-				<tr>
-					<td class="deleteLink"><a href="<?php echo add_query_arg(array('action' => 'delete', 'id' => $quote['id'])); ?>">[x]</a></td>
-					<td class="quote"><?php echo $quote['quote']; ?></td>
-					<td class="owner"><?php echo $quote['owner']; ?></td>
-				</tr>
-				<?php } ?>
-			</thead>
-		</table>
+		<div id="th0ths_quotes_edit_quotes">
+			<form method="post" name="edit_quotes">
+				<table id="th0ths_quotes_qlist" class="widefat">
+					<thead>
+						<tr>
+							<th class="deleteLink"><input type="checkbox" onClick="checkAll('delQuoteCB',this)" /></th>
+							<th>Quote</th>
+							<th>Owner</th>
+						</tr>
+						<?php foreach ($quotes as $quote) { ?>
+						<tr>
+							<td class="deleteLink"><input type="checkbox" class="delQuoteCB" name="delQuoteID[]" value="<?php echo $quote['id']; ?>" /></td>
+							<td class="quote"><?php echo $quote['quote']; ?></td>
+							<td class="owner"><?php echo $quote['owner']; ?></td>
+						</tr>
+						<?php } ?>
+					</thead>
+				</table>
+				<input type="hidden" name="action" value="delQuote" />
+				<input class="button" type="submit" value="Delete Selected Quotes" />
+			</form>
+		</div>
 		<form method="post">
 			<h3 class="hndle">Add new quote</h3>
 			<div id="th0ths_quotes_new_quote" class="postbox">
@@ -149,6 +170,7 @@ function th0ths_quotes_manage_quotes()
 				<input type="text" name="owner" />
 				<div class="th0ths_quotes_cleanser"></div>
 				
+				<input type="hidden" name="action" value="addQuote" />
 				<input class="button" type="submit" value="Add" />
 			</div>
 		</form>
@@ -164,8 +186,11 @@ register_activation_hook(__FILE__, 'th0ths_quotes_activate');
 /* registering administration options */
 add_action('admin_menu', 'th0ths_quotes_add_administration_menus');
 
-/* add plugin stylesheet to wp_head */
+/* add plugin stylesheet to admin_head */
 add_action('admin_head', 'th0ths_quotes_include_css');
+
+/* add js to admin_head */
+add_action('admin_head', 'th0ths_quotes_admin_head_js');
 
 include "th0ths_quotes_widget.php";
 
