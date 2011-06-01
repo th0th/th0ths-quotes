c4d44bcea591709c8d8cd96e30ae148dd5bb9e35
Hüseyin Gökhan Sarı <th0th@returnfalse.net>
2011-06-01 05:17:39 +0300

    now widget is fancier

--- th0ths-quotes-widget.php
+++ th0ths-quotes-widget.php
@@ -33,7 +33,7 @@ class th0ths_Quotes_Widget extends WP_Widget {
                             echo $before_title . $title . $after_title; }
                         else {
                                 echo $before_title . "Quotes" . $after_title; } ?>
-                  <i><?php echo $quote['quote']; ?></i> -<?php echo $quote['owner']; ?>
+                  <div id="th0ths_quotes_widget_quote" style="font-style: oblique;"><?php echo $quote['quote']; ?></div><div id="th0ths_quotes_widget_owner" style="text-align: right;">-<?php echo $quote['owner']; ?></div>
               <?php echo $after_widget; ?>
         <?php
     }
