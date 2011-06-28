125d6d153017e41097535c2fee0f769326506da4
H.Gökhan Sarı <th0th@returnfalse.net>
2011-06-29 00:17:01 +0300

    convert tabs to spaces

--- admin-head.js
+++ admin-head.js
@@ -1,9 +1,9 @@
 function checkAll(cbclass,exby)
 {
-	var cbs = document.getElementsByClassName(cbclass);
-	
-	for ( var i=0; i<cbs.length; i++ )
-	{
-		cbs[i].checked = exby.checked? true:false
-	}
+    var cbs = document.getElementsByClassName(cbclass);
+    
+    for ( var i=0; i<cbs.length; i++ )
+    {
+        cbs[i].checked = exby.checked? true:false
+    }
 }
